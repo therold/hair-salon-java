8dabe9f05e440ecd4638a91352d97d854e31ec5f
Timothy Herold <heroldt@gmail.com>
2016-09-23 09:58:17 -0700

    Adjust App.java to use static model HashMap. User Spark before() directive to clear the map before processing a route each and every time.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,12 +7,15 @@ import static spark.Spark.*;
 public class App {
   private static Client activeClient;
   private static Stylist activeStylist;
+  private static Map<String, Object> model;
 
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    model = new HashMap<String, Object>();
 
     before((request, response) -> {
+      model.clear();
       String referer;
       if(request.headers("Referer") != null) {
         if(request.headers("Referer").contains("?")) {
@@ -20,7 +23,7 @@ public class App {
         } else {
           referer = request.headers("Referer");
         }
-        if(request.requestMethod().equals("GET") && !referer.equals(request.session().attribute("Current"))) {
+        if(!referer.equals(request.session().attribute("Current"))) {
           request.session().attribute("Prev", request.session().attribute("Current"));
           request.session().attribute("Current", referer);
         }
@@ -29,7 +32,6 @@ public class App {
 
     // Home
     get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -37,21 +39,21 @@ public class App {
     // Stylists
     get("/stylists", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/stylists/new", (request, response) -> {
-      //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
+      if (activeStylist != null) {
+        response.redirect("/stylists/profile");
+      } else if (request.queryParams("stylistexists") != null) {
+        model.put("stylistexists", true);
+      }
       model.put("template", "templates/stylists/new.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     post("/stylists", (request, response) -> {
-      //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("name");
       if (Stylist.findByName(name) != null) {
         response.redirect(request.session().attribute("Current") + "?stylistexists=true");
@@ -60,35 +62,37 @@ public class App {
         stylist.save();
         activeClient = null;
         activeStylist = stylist;
-        response.redirect(request.session().attribute("Prev"));
+        response.redirect(request.session().attribute("Current"));
       }
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/stylists/search", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/stylists/signin", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/stylists/signout", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients/profile", (request, response) -> {
+      //TODO
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/stylists/:id", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -96,19 +100,21 @@ public class App {
     // Clients
     get("/clients", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/clients/new", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
+      if (activeClient != null) {
+        response.redirect("/clients/profile");
+      } else if (request.queryParams("clientexists") != null) {
+        model.put("clientexists", true);
+      }
       model.put("template", "templates/clients/new.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     post("/clients", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("name");
       if (Client.findByName(name) != null) {
         response.redirect(request.session().attribute("Current") + "?clientexists=true");
@@ -117,44 +123,46 @@ public class App {
         client.save();
         activeStylist = null;
         activeClient = client;
-        response.redirect(request.session().attribute("Prev"));
+        response.redirect(request.session().attribute("Current"));
       }
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/clients/search", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/clients/signin", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/clients/signout", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients/profile", (request, response) -> {
+      //TODO
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/clients/:id", (request, response) -> {
       //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     // Owner
     get("/owner", (request, response) -> {
-      //TODO
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/index.vtl");
+      activeClient = null;
+      activeStylist = null;
+      model.put("template", "templates/owner.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
